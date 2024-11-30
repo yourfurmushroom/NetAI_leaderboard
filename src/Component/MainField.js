52b52aa9070667652d937e8f9c092ab2ed3a9d71
zihui <zihui_0411@hotmail.com>
2024-11-30 16:43:06 +0800

    add cookie

--- src/Component/MainField.js
+++ src/Component/MainField.js
@@ -1,20 +1,24 @@
 import React from "react";
 import NavBar from "./NavBar";
 import WorkPlace from "./WorkPlace";
+import Cookies from 'js-cookie';
 
 export default class MainField extends React.Component {
     constructor(Props) {
         super(Props)
         this.state = {
-            isLogin: false,
-            userName: "",
+            isLogin: Cookies.get('isLogin') ||false,
+            userName: Cookies.get('userName') || "",
             isCheckSelfBoard:false,
             isModifyProperty:false,
-            groupname:"",
+            groupname:Cookies.get('groupName') ||"",
         }
     }
 
     LoginHandler(name,groupName) {
+        Cookies.set('userName', name, { expires: 7 }); // Cookie 有效期 7 天
+        Cookies.set('groupName', groupName, { expires: 7 });
+        Cookies.set('isLogin',true,{expires:7})
         this.setState({
             isLogin: true,
             userName: name,
@@ -23,9 +27,13 @@ export default class MainField extends React.Component {
     }
     Logout()
     {
+        Cookies.remove('userName');
+        Cookies.remove('groupName');
+        Cookies.remove('isLogin')
         this.setState({
             isLogin: false,
             userName: "",
+            groupName:""
         })
     }
 
@@ -47,16 +55,16 @@ export default class MainField extends React.Component {
 
             return (
                 <div>
-                    <NavBar groupName={this.state.groupName} modifyProperty={()=>this.ChangeUserPropertiesWindow()} isModifyProperty={this.state.isModifyProperty} CheckSelfBoard={()=>this.CheckSelfBoard()} isCheckSelfBoard={this.state.isCheckSelfBoard} isLogin={this.state.isLogin} setLogin={(e,v)=>this.LoginHandler(e,v)} userName={this.state.userName} logout={()=>{this.Logout()}}></NavBar>
-                    <WorkPlace groupName={this.state.groupName} userName={this.state.userName} modifyProperty={()=>this.ChangeUserPropertiesWindow()} isModifyProperty={this.state.isModifyProperty} CheckSelfBoard={()=>this.CheckSelfBoard()} isCheckSelfBoard={this.state.isCheckSelfBoard} isuserName={this.state.userName} isLogin={this.state.isLogin} setLogin={(e)=>this.LoginHandler(e)}></WorkPlace>
+                    <NavBar groupName={this.state.groupname} modifyProperty={()=>this.ChangeUserPropertiesWindow()} isModifyProperty={this.state.isModifyProperty} CheckSelfBoard={()=>this.CheckSelfBoard()} isCheckSelfBoard={this.state.isCheckSelfBoard} isLogin={this.state.isLogin} setLogin={(e,v)=>this.LoginHandler(e,v)} userName={this.state.userName} logout={()=>{this.Logout()}}></NavBar>
+                    <WorkPlace groupName={this.state.groupname} userName={this.state.userName} modifyProperty={()=>this.ChangeUserPropertiesWindow()} isModifyProperty={this.state.isModifyProperty} CheckSelfBoard={()=>this.CheckSelfBoard()} isCheckSelfBoard={this.state.isCheckSelfBoard} isuserName={this.state.userName} isLogin={this.state.isLogin} setLogin={(e)=>this.LoginHandler(e)}></WorkPlace>
                 </div>
             )
         }
         else {
             return (
                 <div>
-                    <NavBar groupName={this.state.groupName} CheckSelfBoard={()=>this.CheckSelfBoard()} isLogin={this.state.isLogin} setLogin={(e,v)=>this.LoginHandler(e,v)} userName={this.state.userName} logout={()=>{this.Logout()}}></NavBar>
-                    <WorkPlace groupName={this.state.groupName} userName={this.state.userName} CheckSelfBoard={()=>this.CheckSelfBoard()} isCheckSelfBoard={this.state.isCheckSelfBoard} isLogin={this.state.isLogin} setLogin={(e)=>this.LoginHandler(e)}></WorkPlace>
+                    <NavBar groupName={this.state.groupname} CheckSelfBoard={()=>this.CheckSelfBoard()} isLogin={this.state.isLogin} setLogin={(e,v)=>this.LoginHandler(e,v)} userName={this.state.userName} logout={()=>{this.Logout()}}></NavBar>
+                    <WorkPlace groupName={this.state.groupname} userName={this.state.userName} CheckSelfBoard={()=>this.CheckSelfBoard()} isCheckSelfBoard={this.state.isCheckSelfBoard} isLogin={this.state.isLogin} setLogin={(e)=>this.LoginHandler(e)}></WorkPlace>
                 </div>
             )
         }
